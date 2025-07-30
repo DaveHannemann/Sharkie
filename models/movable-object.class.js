04b3e873e12ba67146a34e91dd1c6a22315d5fd8
Dave Hannemann <david.dikschat@web.de>
2025-07-30 19:25:04 +0200

    first commit

---
+++ models/movable-object.class.js
@@ -0,0 +1,23 @@
+class MovableObject{
+    x = 120;
+    y = 300;
+    img;
+    width = 150;
+    height = 100;
+
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight () {
+        console.log('Moving right');
+    }
+
+    moveLeft(){
+        console.log('Moving left');
+    }
+
+}
---
+++ models/world.class.js
@@ -0,0 +1,38 @@
+class World {
+    charakter = new Character();
+    enemies = [
+        new Fish(),
+        new Fish(),
+        new Fish(),
+    ];
+    water = [
+        new Water()
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas){
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+
+
+    draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.charakter.img, this.charakter.x, this.charakter.y, this.charakter.width, this.charakter.height);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+        this.water.forEach(w => {
+            this.ctx.drawImage(w.img, w.x, w.y, w.width, w.height);
+        });
+
+        // draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
+    }
+}
